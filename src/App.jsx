f88cf3bfd082c6e055b8d937208cdae9d1e4d660
AyanChak3 <>
2024-07-16 10:47:25 +0530

    Added Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
+import Layout from './Layouts/Layout'
 import Home from './Pages/Home'
 
 function App() {
 
   return (
     <div>
-       <Home />
+      <Layout>
+        <Home />
+      </Layout>
     </div>
   )
 }
-
 export default App
